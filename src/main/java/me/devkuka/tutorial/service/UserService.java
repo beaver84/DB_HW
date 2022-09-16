0fe481a568d925a45104c58a4994ad28ec335a73
KyungTae Bae <kyungtae@marvrus.com>
2022-09-16 18:14:10 +0900

    JWT 로그인까지 (5강 - 완료)

---
+++ src/main/java/me/devkuka/tutorial/service/UserService.java
@@ -0,0 +1,60 @@
+package me.devkuka.tutorial.service;
+
+import java.util.Collections;
+import java.util.Optional;
+import me.devkuka.tutorial.dto.UserDto;
+import me.devkuka.tutorial.entity.Authority;
+import me.devkuka.tutorial.entity.Users;
+import me.devkuka.tutorial.exception.DuplicateMemberException;
+import me.devkuka.tutorial.exception.NotFoundMemberException;
+import me.devkuka.tutorial.repository.UserRepository;
+import me.devkuka.tutorial.util.SecurityUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public UserDto signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
+            throw new DuplicateMemberException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        Users user = Users.builder()
+                .username(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return UserDto.from(userRepository.save(user));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getUserWithAuthorities(String username) {
+        return UserDto.from(userRepository.findOneWithAuthoritiesByUsername(username).orElse(null));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getMyUserWithAuthorities() {
+        return UserDto.from(
+                SecurityUtil.getCurrentUsername()
+                        .flatMap(userRepository::findOneWithAuthoritiesByUsername)
+                        .orElseThrow(() -> new NotFoundMemberException("Member not found"))
+        );
+    }
+}
