e3132b274305304e48100da3391441daca869869
baekt <junkaiji@gmail.com>
2022-09-27 10:55:07 +0900

    validation 추가

--- src/main/java/me/devkuka/tutorial/controller/MyRoomController.java
+++ src/main/java/me/devkuka/tutorial/controller/MyRoomController.java
@@ -3,10 +3,14 @@ package me.devkuka.tutorial.controller;
 import lombok.RequiredArgsConstructor;
 import me.devkuka.tutorial.dto.*;
 import me.devkuka.tutorial.service.MyRoomService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
+@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api")
@@ -22,14 +26,18 @@ public class MyRoomController {
 
     @GetMapping("/v1/my-room/username/{username}")
     public List<MyRoomResponse> getAllMyRooms(
-            @PathVariable String username
+            @PathVariable
+            @NotEmpty(message = "유저 이름을 입력해주세요.")
+            String username
     ) {
         return myRoomService.getAllMyRooms(username);
     }
 
     @GetMapping("/v1/my-room/id/{myRoomId}")
     public MyRoomResponse getMyRoomById(
-            @PathVariable long myRoomId
+            @PathVariable
+            @Min(value = 1, message = "마이룸 ID를 입력해주세요.")
+            long myRoomId
     ) {
         return myRoomService.getMyRoomById(myRoomId);
     }
@@ -45,6 +53,7 @@ public class MyRoomController {
 
     @PutMapping("/v1/my-room")
     public MyRoomModifyResponse modifyMyRoom(
+            @Min(value = 1, message = "마이룸 ID를 입력해주세요.")
             @RequestParam long myRoomId,
             @RequestBody MyRoomModifyRequest myRoomModifyRequest
     ) {
@@ -53,6 +62,7 @@ public class MyRoomController {
 
     @DeleteMapping("/v1/my-room")
     public void deleteMyRoom(
+            @Min(value = 1, message = "마이룸 ID를 입력해주세요.")
             @RequestParam long myRoomId
     ) {
         myRoomService.deleteMyRoom(myRoomId);
